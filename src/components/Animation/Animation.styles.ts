c73a4aa1c13557ec4a0e869a775240a0299fe618
Batuhan Orhan <bthnorhan00@gmail.com>
2025-01-21 16:52:33 +0300

    fix: image fill screen

--- src/components/Animation/Animation.styles.ts
+++ src/components/Animation/Animation.styles.ts
@@ -1,13 +1,11 @@
 import { StyleSheet } from 'react-native';
 
-export default StyleSheet.create( {
-  container: StyleSheet.absoluteFillObject,
-  absolute: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-  },
-  cube: {
-    justifyContent: 'center',
-  },
-} );
+export default StyleSheet.create({
+	container: StyleSheet.absoluteFillObject,
+	absolute: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+	},
+	cube: {},
+});
--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,7 +1,10 @@
 import { Image, View } from 'react-native';
 import React, { FC, memo, useState } from 'react';
 import {
-  runOnJS, useAnimatedReaction, useDerivedValue, useSharedValue,
+	runOnJS,
+	useAnimatedReaction,
+	useDerivedValue,
+	useSharedValue,
 } from 'react-native-reanimated';
 import { StoryImageProps } from '../../core/dto/componentsDTO';
 import Loader from '../Loader';
@@ -10,120 +13,116 @@ import ImageStyles from './Image.styles';
 import StoryVideo from './video';
 import { StoryItemProps } from '../../core/dto/instagramStoriesDTO';
 
-const StoryImage: FC<StoryImageProps> = ( {
-  stories, activeStory, defaultStory, isDefaultVideo, paused, videoProps, isActive,
-  mediaContainerStyle, imageStyles, imageProps, videoDuration, onImageLayout, onLoad,
-} ) => {
+const StoryImage: FC<StoryImageProps> = ({
+	stories,
+	activeStory,
+	defaultStory,
+	isDefaultVideo,
+	paused,
+	videoProps,
+	isActive,
+	mediaContainerStyle,
+	imageStyles,
+	imageProps,
+	videoDuration,
+	onImageLayout,
+	onLoad,
+}) => {
+	const [data, setData] = useState<{ data?: StoryItemProps; isVideo?: boolean }>({
+		data: defaultStory,
+		isVideo: isDefaultVideo,
+	});
 
-  const [ data, setData ] = useState<{ data?: StoryItemProps, isVideo?: boolean }>(
-    { data: defaultStory, isVideo: isDefaultVideo },
-  );
+	const loading = useSharedValue(true);
+	const color = useSharedValue(LOADER_COLORS);
+	const duration = useSharedValue<number | undefined>(undefined);
+	const isPaused = useDerivedValue(() => paused.value || !isActive.value);
 
-  const loading = useSharedValue( true );
-  const color = useSharedValue( LOADER_COLORS );
-  const duration = useSharedValue<number | undefined>( undefined );
-  const isPaused = useDerivedValue( () => paused.value || !isActive.value );
+	const onImageChange = async () => {
+		if (!activeStory.value) {
+			return;
+		}
 
-  const onImageChange = async () => {
+		const story = stories.find(item => item.id === activeStory.value);
 
-    if ( !activeStory.value ) {
+		if (!story) {
+			return;
+		}
 
-      return;
+		if (data.data?.id === story.id) {
+			if (!loading.value) {
+				onLoad(duration.value);
+			}
+		} else {
+			loading.value = true;
+			setData({ data: story, isVideo: story.mediaType === 'video' });
+		}
 
-    }
+		const nextStory = stories[stories.indexOf(story) + 1];
 
-    const story = stories.find( ( item ) => item.id === activeStory.value );
+		if (nextStory && nextStory.mediaType !== 'video' && (nextStory.source as any)?.uri) {
+			Image.prefetch((nextStory.source as any)?.uri);
+		}
+	};
 
-    if ( !story ) {
+	useAnimatedReaction(
+		() => isActive.value,
+		(res, prev) => res !== prev && res && runOnJS(onImageChange)(),
+		[isActive.value, onImageChange],
+	);
 
-      return;
+	useAnimatedReaction(
+		() => activeStory.value,
+		(res, prev) => res !== prev && runOnJS(onImageChange)(),
+		[activeStory.value, onImageChange],
+	);
 
-    }
+	const onContentLoad = (newDuration?: number) => {
+		const animationDuration =
+			(data?.data?.mediaType === 'video' ? videoDuration : undefined) ??
+			data.data?.animationDuration ??
+			newDuration;
+		duration.value = animationDuration;
 
-    if ( data.data?.id === story.id ) {
+		loading.value = false;
 
-      if ( !loading.value ) {
+		if (isActive.value) {
+			onLoad(animationDuration);
+		}
+	};
 
-        onLoad( duration.value );
-
-      }
-
-    } else {
-
-      loading.value = true;
-      setData( { data: story, isVideo: story.mediaType === 'video' } );
-
-    }
-
-    const nextStory = stories[stories.indexOf( story ) + 1];
-
-    if ( nextStory && nextStory.mediaType !== 'video' && ( nextStory.source as any )?.uri ) {
-
-      Image.prefetch( ( nextStory.source as any )?.uri );
-
-    }
-
-  };
-
-  useAnimatedReaction(
-    () => isActive.value,
-    ( res, prev ) => res !== prev && res && runOnJS( onImageChange )(),
-    [ isActive.value, onImageChange ],
-  );
-
-  useAnimatedReaction(
-    () => activeStory.value,
-    ( res, prev ) => res !== prev && runOnJS( onImageChange )(),
-    [ activeStory.value, onImageChange ],
-  );
-
-  const onContentLoad = ( newDuration?: number ) => {
-
-    const animationDuration = ( data?.data?.mediaType === 'video' ? videoDuration : undefined ) ?? data.data?.animationDuration ?? newDuration;
-    duration.value = animationDuration;
-
-    loading.value = false;
-
-    if ( isActive.value ) {
-
-      onLoad( animationDuration );
-
-    }
-
-  };
-
-  return (
-    <>
-      <View style={ImageStyles.container}>
-        <Loader loading={loading} color={color} size={50} />
-      </View>
-      <View style={[ ImageStyles.image, mediaContainerStyle ]}>
-        {data.data?.source && (
-          data.isVideo ? (
-            <StoryVideo
-              onLoad={onContentLoad}
-              onLayout={onImageLayout}
-              source={data.data.source}
-              paused={isPaused}
-              isActive={isActive}
-              {...videoProps}
-            />
-          ) : (
-            <Image
-              source={data.data.source}
-              style={[ { width: WIDTH, aspectRatio: 0.5626 }, imageStyles ]}
-              resizeMode="contain"
-              testID="storyImageComponent"
-              onLayout={( e ) => onImageLayout( Math.min( HEIGHT, e.nativeEvent.layout.height ) )}
-              onLoad={() => onContentLoad()}
-              {...imageProps}
-            />
-          )
-        )}
-      </View>
-    </>
-  );
-
+	return (
+		<>
+			<View style={ImageStyles.container}>
+				<Loader loading={loading} color={color} size={50} />
+			</View>
+			<View style={[ImageStyles.image, mediaContainerStyle]}>
+				{data.data?.source &&
+					(data.isVideo ? (
+						<StoryVideo
+							onLoad={onContentLoad}
+							onLayout={onImageLayout}
+							source={data.data.source}
+							paused={isPaused}
+							isActive={isActive}
+							{...videoProps}
+						/>
+					) : (
+						<Image
+							source={data.data.source}
+							style={[{ width: WIDTH, height: '100%' }, imageStyles]}
+							resizeMode='cover'
+							testID='storyImageComponent'
+							onLayout={e =>
+								onImageLayout(Math.min(HEIGHT, e.nativeEvent.layout.height))
+							}
+							onLoad={() => onContentLoad()}
+							{...imageProps}
+						/>
+					))}
+			</View>
+		</>
+	);
 };
 
-export default memo( StoryImage );
+export default memo(StoryImage);
